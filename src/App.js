db239c303ea6aa6056d953ff9a981ce836761e09
Riya Singh <riya.singh@magicpin.in>
2024-03-05 12:42:37 +0530

    digital clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [time, updateTime] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      updateTime(new Date())
+    }, 1000);
+  
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h4>Digital Clock</h4>
       </header>
+      <div className='clock-div'>
+        <h1>{time.toLocaleTimeString()}</h1>
+      </div>
     </div>
   );
 }
